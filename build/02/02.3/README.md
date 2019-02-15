9dbcc19c352dc48d3933aedcb6791b9b40a35140
larryzodiac <evanmachale@gmail.com>
2019-02-15 16:13:39 +0000

    Build 02.3 > Setup

---
+++ build/02/02.3/README.md
@@ -0,0 +1,9 @@
+# Generative Jewellery ~ Semester 2 ~ MERN
+
+Building a full stack.
+
+0. [...]().
+
+## Reference
+
+To be added..
---
+++ build/02/02.3/src/App.js
@@ -0,0 +1,33 @@
+// ------------------------------------------------- //
+// Evan MacHale - N00150552
+// 15.02.19
+// ------------------------------------------------- //
+
+import React, { Component } from 'react';
+// Material Design Components
+import { Cell, Grid, Row } from '@material/react-layout-grid';
+
+// ------------------------------------------------- //
+
+// My Components
+
+// ------------------------------------------------- //
+
+import './App.scss';
+
+// ------------------------------------------------- //
+
+// Most of our material components will be compiled here alongside our canvas... I hope
+class App extends Component {
+  render() {
+    return (
+      <Grid>Hello</Grid>
+    );
+  }
+}
+
+// ------------------------------------------------- //
+
+export default App;
+
+// ------------------------------------------------- //
