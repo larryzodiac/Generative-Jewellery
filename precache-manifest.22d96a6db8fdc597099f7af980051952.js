35a58e4d62c40edea023d73e21d80c31bf5e8d72
larryzodiac <evanmachale@gmail.com>
2019-02-26 17:20:31 +0000

    Updates

---
+++ precache-manifest.22d96a6db8fdc597099f7af980051952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e932ea196caea30050d",
+    "url": "/generative-jewellery/static/js/runtime~main.5e932ea1.js"
+  },
+  {
+    "revision": "ee0d0a481b2d9e2f09b0",
+    "url": "/generative-jewellery/static/js/main.ee0d0a48.chunk.js"
+  },
+  {
+    "revision": "7cbfadf5a7ce88ac9a27",
+    "url": "/generative-jewellery/static/js/2.7cbfadf5.chunk.js"
+  },
+  {
+    "revision": "ee0d0a481b2d9e2f09b0",
+    "url": "/generative-jewellery/static/css/main.bfa75c79.chunk.css"
+  },
+  {
+    "revision": "6ce73ebaedcaa8c897acd9d2696df412",
+    "url": "/generative-jewellery/index.html"
+  }
+];
